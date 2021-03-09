7c20208d2433b7651b205a9528507c75650fe92d
Ameng <1831799277@qq.com>
2021-03-09 15:41:30 +0800

    解决windows终端颜色问题，增加linux程序

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module awesomeProject2
 
 go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	golang.org/x/sys v0.0.0-20210309040221-94ec62e08169 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"awesomeProject2/Color"
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
+	"github.com/fatih/color"
 	"net"
 	"os"
 	"sync"
@@ -26,12 +25,13 @@ func init(){
 	flag.StringVar(&outputfile,"o","","要输出的文件")
 	flag.Usage = usage
 	flag.Parse()
+
 }
 
 func usage(){
-	fmt.Fprintf(os.Stderr,`Ameng编写的Go语言域名解析工具
+	fmt.Fprintf(color.Output,color.HiCyanString(`Ameng编写的Go语言域名解析工具
 Options:
-`)
+`))
 	flag.PrintDefaults()
 }
 
@@ -41,7 +41,7 @@ func Getip(inputfile string, outputfile string)(count1 int, count2 int){
 	infile, err := os.OpenFile(inputfile,os.O_RDONLY,1)
 	outfile, err := os.OpenFile(outputfile,os.O_WRONLY,2)
 	if err != nil {
-		log.Println(Color.Red("[ERROR]")+":",err)
+		fmt.Fprintln(color.Output,time.Now().Format("2006/01/02 12:22:48"),color.RedString("[ERROR]")+":",err)
 	}
 	hostnames := bufio.NewScanner(infile)
 	var wg sync.WaitGroup
@@ -54,7 +54,7 @@ func Getip(inputfile string, outputfile string)(count1 int, count2 int){
 			defer wg.Done()
 			ip, err := net.ResolveIPAddr("ip",hostname)
 			if err != nil {
-				log.Println(Color.Yellow("[WARNING]")+":",err)
+				fmt.Fprintln(color.Output,time.Now().Format("2006/01/02 12:22:48"),color.YellowString("[WARNING]")+":",err)
 			}
 			if ip != nil {
 				ips = append(ips, ip.String())
@@ -89,7 +89,7 @@ func main(){
 		start := time.Now()
 		count1,count2 := Getip(inputfile,outputfile)
 		end := time.Since(start)
-		defer fmt.Print(Color.Green("域名解析已完成,总用时:"),end,Color.Green(" 解析域名数:"),count1,Color.Green(" 获得ip数:"),count2)
+		defer fmt.Fprint(color.Output,color.GreenString("域名解析已完成,总用时:"),end,color.GreenString(" 解析域名数:"),count1,color.GreenString(" 获得ip数:"),count2)
 	}
 
 }
